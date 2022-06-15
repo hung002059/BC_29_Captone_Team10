e187f4105aaed4739f46280a047aa843e7eb7348
hung002059 <102804596+hung002059@users.noreply.github.com>
2022-06-15 14:53:26 +0700

    upload file

---
+++ models/product.js
@@ -0,0 +1,23 @@
+function SanPham(
+  id,
+  name,
+  price,
+  screen,
+  backCamera,
+  frontCamera,
+  img,
+  desc,
+  type,
+  soLuong
+) {
+  this.id = id;
+  this.name = name;
+  this.price = price;
+  this.screen = screen;
+  this.backCamera = backCamera;
+  this.frontCamera = frontCamera;
+  this.img = img;
+  this.desc = desc;
+  this.type = type;
+  this.soLuong = soLuong;
+}
